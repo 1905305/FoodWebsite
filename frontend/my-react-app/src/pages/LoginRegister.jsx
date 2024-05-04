1521d3513d765a5b118d3c2e895c0b592a8eb19f
1905305 <1905305@kiit.ac.in>
2024-05-04 16:59:34 -0400

    changes made in files found

--- frontend/my-react-app/src/pages/LoginRegister.jsx
+++ frontend/my-react-app/src/pages/LoginRegister.jsx
@@ -31,7 +31,7 @@ const LoginRegister = () => {
   const registerUser = async (currentUser) => {
     try {
       const response = await axios.post(
-        `http://https://https://food-website-tau-topaz.vercel.app//api/signup`,
+        `https://food-website-tau-topaz.vercel.app/api/signup`,
         currentUser
       );
       const { user, token } = response.data;
@@ -49,7 +49,7 @@ const LoginRegister = () => {
   const loginUser = async (currentUser) => {
     try {
       const response = await axios.post(
-        `http://https://https://food-website-tau-topaz.vercel.app//api/login`,
+        `https://food-website-tau-topaz.vercel.app/api/login`,
         currentUser
       );
       const { user, token } = response.data;
